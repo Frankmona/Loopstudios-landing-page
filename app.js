0f27e9b665cbb30fe93f029f309f27bfa711e100
Frankmona <frankoguche1@gmail.com>
2023-08-12 18:27:43 +0100

    completed desktop screen

---
+++ app.js
@@ -0,0 +1,7 @@
+const currentYear = document.getElementById('current-year')
+const year = new Date().getFullYear();
+
+currentYear.textContent = `© ${year} Loopstudios. All rights reserved.`
+
+// const  = new Date().getFullYear();
+// document.getElementById('current-year').textContent = currentYear;
